de2c005acc3fb89cb4a13569e1381f40ed00bf6f
Qayyum Yazid <qay.yaz19@gmail.com>
2018-01-06 12:16:59 +0000

    pencil says yeah!

---
+++ Assets/WinSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinSound : MonoBehaviour {
+
+	public AudioSource audio;
+	public AudioClip pencil;
+
+	public void Pencil(){
+
+		audio.PlayOneShot (pencil);
+
+	}
+}
